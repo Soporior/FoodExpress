e62199ca6cc99b29a45f9f20fd47e3f2545c3469
Soporior <44942622+Soporior@users.noreply.github.com>
2021-07-14 10:11:47 +0800

    shop list scroll

--- src/pages/Shop/ShopGoods/ShopGoods.vue
+++ src/pages/Shop/ShopGoods/ShopGoods.vue
@@ -1,15 +1,235 @@
 <template>
-<div>goods</div>
-</template>
 
+    <div class="goods">
+      <div class="menu-wrapper" >
+        <ul>
+          <li class="menu-item " v-for="(good,index) in goods" :key="index" :class="{current:index===currentIndex}"
+                        @click="clickMenuItem(index)">
+            <span class="text bottom-border-1px">
+            <img class="icon" :src="good.icon" v-if="good.icon"> {{ good.name }} </span>
+          </li>
+
+        </ul>
+      </div>
+
+
+      <div class="foods-wrapper">
+        <ul ref="foodUl">
+
+          <li class="food-list-hook" v-for="(good,index) in goods" :key="index">
+            <h1 class="title">{{ good.name }}</h1>
+            <ul>
+              <li class="food-item bottom-border-1px" v-for="(food,index) in good.foods">
+                <div class="icon">
+                  <img width="57" height="57" :src="food.image">
+                </div>
+                <div class="content"><h2 class="name">{{ food.name }}</h2>
+                  <p class="desc">{{ food.description }}</p>
+                  <div class="extra"><span class="count">月售 {{ food.sellCount }} 份</span>
+                    <span>好评率 {{ food.rating }}%</span></div>
+                  <div class="price">
+                    <span class="now">￥{{food.price}}</span>
+                    <span class="old" v-if="food.oldPrice">￥{{food.oldPrice}}</span>
+                  </div>
+                  <div class="cartcontrol-wrapper"> CartControl</div>
+                </div>
+              </li>
+
+            </ul>
+          </li>
+
+        </ul>
+      </div>
+
+
+    </div>
+
+</template>
 <script>
+import {mapState} from 'vuex'
+import BScroll from 'better-scroll'
+
 export default {
-mounted() {
-  this.$store.dispatch('getShopGoods')
-}
+  data(){
+    return{
+      tops:[],
+      food:0,
+      scrollY:0
+    }
+  },
+
+  mounted() {
+
+  this.$store.dispatch('getShopGoods',()=>{ //数据更新后执行
+      this.$nextTick(()=>{//列表更新显示后执行
+       //列表显示后创建
+       this._initScroll()
+        this._initTops()
+        })
+  })
+
+
+  },
+  computed:{
+    ...mapState(['goods']),
+    currentIndex(){
+      const {scrollY,tops} = this
+
+      const index = tops.findIndex((top,index)=>{
+          return scrollY>=top && scrollY<tops[index+1]
+      })
+
+      return index
+    }
+
+  },
+  methods:{
+    //初始化滚动
+    _initScroll(){
+      new BScroll('.menu-wrapper',{
+        //betterScroll默认会阻止点击时间，下面操作开启点击事件
+        click:true
+      })
+      const foodScroll = new BScroll('.foods-wrapper',{
+        click:true,
+        probeType: 2,  // 因为惯性滑动不会触发
+      })
+      this.foodScroll = foodScroll
+      //给右侧绑定滑动监听
+      foodScroll.on('scroll',({x, y})=>{
+
+        this.scrollY = Math.abs(y)
+
+      })
+      //给右侧绑定滑动结束的监听
+      foodScroll.on('scrollEnd',({x,y})=>{
+        this.scrollY = Math.abs(y)
+      })
+    },
+    //初始化tops
+    _initTops(){
+      //初始化tops
+     const tops = [0]
+      //设定一个top的初始值
+      let top = 0
+      //获取所有分类的li  原始方法 document.getElementsByClassName('food-list-hook')
+      const lis = this.$refs.foodUl.children
+      //将获取的分类li的伪数组转化成真数组遍历
+      Array.prototype.slice.call(lis).forEach(li=>{
+      //每遍历一个数组  top的值加上上一个li的可见区域高
+      top += li.clientHeight
+       //把每次处理后的top添加到tops中
+        tops.push(top)
+                                             })
+         //获得每一个记录着每一个li高度的数组
+        this.tops= tops
+
+      },
+    //给右边菜单绑定事件
+    clickMenuItem(index){
+      this.foodScroll.scrollTo(0,-this.tops[index],300)
+    }
+
+  }
+
 }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+@import "../../../common/stylus/mixins.styl"
+.goods
+  display: flex
+  position: absolute
+  top: 195px
+  bottom: 46px
+  width: 100%
+  background: #fff;
+  overflow: hidden
+  .menu-wrapper
+    flex: 0 0 80px
+    width: 80px
+    background: #f3f5f7
+    .menu-item
+      display: table
+      height: 54px
+      width: 56px
+      padding: 0 12px
+      line-height: 14px
+      &.current
+        position: relative
+        z-index: 10
+        margin-top: -1px
+        background: #fff
+        color: $green
+        font-weight: 700
+        .text
+          border-none()
+      .icon
+        display: inline-block
+        vertical-align: top
+        width: 12px
+        height: 12px
+        margin-right: 2px
+        background-size: 12px 12px
+        background-repeat: no-repeat
+      .text
+        display: table-cell
+        width: 56px
+        vertical-align: middle
+        bottom-border-1px(rgba(7, 17, 27, 0.1))
+        font-size: 12px
+  .foods-wrapper
+    flex: 1
+    .title
+      padding-left: 14px
+      height: 26px
+      line-height: 26px
+      border-left: 2px solid #d9dde1
+      font-size: 12px
+      color: rgb(147, 153, 159)
+      background: #f3f5f7
+    .food-item
+      display: flex
+      margin: 18px
+      padding-bottom: 18px
+      bottom-border-1px(rgba(7, 17, 27, 0.1))
+      &:last-child
+        border-none()
+        margin-bottom: 0
+      .icon
+        flex: 0 0 57px
+        margin-right: 10px
+      .content
+        flex: 1
+        .name
+          margin: 2px 0 8px 0
+          height: 14px
+          line-height: 14px
+          font-size: 14px
+          color: rgb(7, 17, 27)
+        .desc, .extra
+          line-height: 10px
+          font-size: 10px
+          color: rgb(147, 153, 159)
+        .desc
+          line-height: 12px
+          margin-bottom: 8px
+        .extra
+          .count
+            margin-right: 12px
+        .price
+          font-weight: 700
+          line-height: 24px
+          .now
+            margin-right: 8px
+            font-size: 14px
+            color: rgb(240, 20, 20)
+          .old
+            text-decoration: line-through
+            font-size: 10px
+            color: rgb(147, 153, 159)
+        .cartcontrol-wrapper
+          position: absolute
+          right: 0
+          bottom: 12px
 </style>
