851e7d33de747bc91680ed1c111e4deeca0ed306
Soporior <44942622+Soporior@users.noreply.github.com>
2021-08-11 11:30:02 +0800

    注释

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -3,26 +3,26 @@
 * 返回值：promise对象
 * */
 import axios from 'axios'
-export default function ajax(url='',data={},type='GET'){
-  return new Promise(function (resolve,reject){
+export default function ajax(url = '', data = {}, type = 'GET') {
+  return new Promise(function (resolve, reject) {
     let promise
-    if(type==='GET'){
-
+    if (type === 'GET') {
+     //将data的数据拼进url中
       let dataStr = ''
-      Object.keys(data).forEach(key=>{
-        dataStr +=key + '=' +data[key] + '&'
+      Object.keys(data).forEach(key => {
+        dataStr += key + '=' + data[key] + '&'
       })
-      if(dataStr !==''){
-        dataStr = dataStr.substring(0,dataStr.lastIndexOf('&'))
+      if (dataStr !== '') {
+        dataStr = dataStr.substring(0, dataStr.lastIndexOf('&'))
         url = url + '?' + dataStr
       }
       promise = axios.get(url)
-    }else {
-      promise = axios.post(url,data)
+    } else {
+      promise = axios.post(url, data)
     }
-    promise.then(response=>{
+    promise.then(response => {
       resolve(response.data)
-    }).catch(error=>{
+    }).catch(error => {
       reject(error)
     })
   })
